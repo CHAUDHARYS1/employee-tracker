714c99b91c25643bb851e61d27492ee821d66dd4
Shital Chaudhary <shitalchau10@gmail.com>
2022-08-20 16:48:31 -0500

    show a single employee

--- routes/apiRoutes/employeesRoutes.js
+++ routes/apiRoutes/employeesRoutes.js
@@ -20,6 +20,25 @@ router.get('/employees', (req, res) => {
     });
 });
 
+// show a single empoloyee
+router.get('/employees/:id', (req, res) => {
+    const sql = `SELECT * FROM employees WHERE id = ?`;
+    const params = [req.params.id];
+    db.query(sql,params, (err, row) => {
+        if(err) {
+            res.status(400).json({
+                error:err.message
+            });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: row
+        });
+    });
+});
+
+
 
 
 module.exports = router;
