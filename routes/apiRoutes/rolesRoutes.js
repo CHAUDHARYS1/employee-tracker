376e495b57129ff3c88b26c25b782bac173536e5
Shital Chaudhary <shitalchau10@gmail.com>
2022-08-21 10:34:31 -0500

    show a single role

--- routes/apiRoutes/rolesRoutes.js
+++ routes/apiRoutes/rolesRoutes.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const db = require('../../db/connection');
 
 // show all roles
-
 router.get('/roles/', (req, res) => {
     const sql = `SELECT * FROM roles`;
     db.query(sql, (err, rows) => {
@@ -20,6 +19,24 @@ router.get('/roles/', (req, res) => {
     });
 });
 
+
+// show a single role
+router.get('/roles/:id', (req, res) => {
+    const sql = `SELECT * FROM roles WHERE id = ?`;
+    const params = [req.params.id];
+    db.query(sql, params, (err, row) => {
+        if (err) {
+            res.status(400).json({
+                error: err.message
+            });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: row
+        });
+    });
+});
 
 
 
